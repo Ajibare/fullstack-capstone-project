0f6f63bd351254388a8de08294b70065242fe696
Ajibare <ajibare.jide@gmail.com>
2025-07-18 11:55:27 -0400

    worked on  the appco ntext page

--- giftlink-frontend/src/context/AuthContext.js
+++ giftlink-frontend/src/context/AuthContext.js
@@ -1,5 +1,23 @@
+// import React, { createContext, useState, useContext } from 'react';
+// import { useAppContext } from '../../context/AuthContext';
+
+// const AppContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [userName, setUserName] = useState("");
+
+//   return (
+//     <AppContext.Provider value={{ isLoggedIn, setIsLoggedIn, userName, setUserName }}>
+//       {children}
+//     </AppContext.Provider>
+//   );
+// };
+
+// export const useAppContext = () => useContext(AppContext);
+
+
 import React, { createContext, useState, useContext } from 'react';
-import { useAppContext } from '../../context/AuthContext';
 
 const AppContext = createContext();
 
